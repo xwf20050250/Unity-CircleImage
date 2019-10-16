95c39a2d3bef794745da6c0ff12a63b812784868
xuweifei <c_iron@126.com>
2019-10-16 16:04:12 +0800

    添加Button测试脚本

---
+++ CircleImage/Assets/main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour {
+
+    public Button m_btn;
+
+	// Use this for initialization
+	void Start () {
+        m_btn.onClick.AddListener(OnBtnClicked);
+    }
+	
+	// Update is called once per frame
+	void OnBtnClicked() {
+        Debug.LogErrorFormat("button click...");
+	}
+}
